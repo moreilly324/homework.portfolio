35b26ff45e079b90c0f280213b2841b94c0cde33
moreilly324 <30608353+moreilly324@users.noreply.github.com>
2017-09-18 16:10:23 -0400

    HW

--- homework-city/js/index.js
+++ homework-city/js/index.js
@@ -9,17 +9,18 @@
 //Images must be hidden upon refresh
 
 function changeBackground() {
-var tag = document.getElementById("city-type").value;
-if (tag == "LA" || tag == "Los Angeles" || tag =="LAX") {
-document.body.style.backgroundImage = "url('images/la.jpg')";
-} else if (tag == "Austin" || tag == "ATX") {
-document.body.style.backgroundImage = "url('images/austin.jpg')";
-} else if (tag == "NYC" || tag == "New York City" || tag == "New York") {
-document.body.style.backgroundImage = "url('images/nyc.jpg')";
-} else if (tag == "SF" || tag == "San Francisco" || tag == "Bay Area") {
-document.body.style.backgroundImage = "url('images/sf.jpg')";
-} else if (tag == "Sydney" || tag == "SYD") {
-document.body.style.backgroundImage = "url('images/sydney.jpg')";
-} else
-alert("tag did not match")
-}
+
+	var tag = document.getElementById("city-type").value;
+	if (tag == "LA" || tag == "Los Angeles" || tag =="LAX") {
+	document.body.style.backgroundImage = "url('images/la.jpg')";
+	} else if (tag == "Austin" || tag == "ATX") {
+	document.body.style.backgroundImage = "url('images/austin.jpg')";
+	} else if (tag == "NYC" || tag == "New York City" || tag == "New York") {
+	document.body.style.backgroundImage = "url('images/nyc.jpg')";
+	} else if (tag == "SF" || tag == "San Francisco" || tag == "Bay Area") {
+	document.body.style.backgroundImage = "url('images/sf.jpg')";
+	} else if (tag == "Sydney" || tag == "SYD") {
+	document.body.style.backgroundImage = "url('images/sydney.jpg')";
+	} else
+	alert("tag did not match")
+	}
